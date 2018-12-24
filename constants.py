1080da186583676d9e79ca31b681dd8541ce1d14
deepexsense <dashka2h@yandex.ru>
2018-12-24 22:38:15 +0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+TELEBOT_TOKEN = '768010442:AAHItr6E0TYmNlTiXp2HHIFbV41KnSCS_vk'
+MAIN_URL = f'https://api.telegram.org/bot{TELEBOT_TOKEN}'
+STICKER_ID = 'CAADAgADggIAAlwohgj91172rg2WVAI'
+photo1 = 'templates/Photos/welcome_img.jpg'
+photo2 = 'templates/Photos/bars_info.jpg'
+photo3 = 'templates/Photos/connect.jpg'
+doc1 = 'templates/Docs/Согласие.docx'
---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import constants
+
+bot = telebot.TeleBot(constants.TELEBOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    all_files = {constants.photo1, constants.photo2, constants.photo3}
+    user_markup.row('Я готов пройти опрос!')
+    user_markup.row('/stop')
+    bot.send_message(message.from_user.id, 'БАРС Груп приветствует тебя!', reply_markup=user_markup)
+    for file in all_files:
+        img = open(file, 'rb')
+        bot.send_chat_action(message.from_user.id, 'upload_photo')
+        bot.send_photo(message.from_user.id, img)
+        img.close()
+
+@bot.message_handler(content_types=['text'])
+def handle_start(message):
+    if message.text == "Я готов пройти опрос!":
+        user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+        all_files = {constants.doc1}
+        user_markup.row('Принять')
+        user_markup.row('/stop')
+        bot.send_message(message.from_user.id, 'Тогда прочитай и прими '
+                                               'согласие на обработку персональных данных', reply_markup=user_markup)
+        for file in all_files:
+            doc = open(file, 'rb')
+            bot.send_chat_action(message.from_user.id, 'upload_document')
+            bot.send_document(message.from_user.id, doc)
+            doc.close()
+
+bot.polling(none_stop=True, interval=0)
